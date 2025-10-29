26b33226eab17fc301f2ef60774889b979b02e7d
Superpro <91602978+ProgrammerLinuxRus@users.noreply.github.com>
2025-10-29 23:29:57 +0500

    Add files via upload

---
+++ .config/qtile/colors.py
@@ -0,0 +1,12 @@
+colors = {
+    'bg': '#181825', #background
+    'bg-alt': 'f9e2af', #background and foreground for time and window name moudules
+    'fg': '#fdefcc', #foregorund for right modules
+    'active': '#c6ac70',
+    'inactive': '#fdefcc',
+    'current': '#ef9f76',
+    'ab': '#c6ac70',
+    'ib' : '#917d50'
+
+
+}
